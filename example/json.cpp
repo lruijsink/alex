0a9a2ade9a7373a863a6e0c3dd19faf80453a393
Luc <lruijsink@gmail.com>
2020-03-25 00:45:57 +0100

    Tidied up JSON example

--- example/json.cpp
+++ example/json.cpp
@@ -1,53 +1,52 @@
-#include <string_view>
 #include <iostream>
 #include "alex/alex.h"
 
+using namespace alex;
+
 int main()
 {
-  auto ws_char        = alex::one_of(' ', '\t', '\n', 'r');
-  auto ws             = alex::repeat(ws_char);
+  auto ws             = repeat(one_of(' ', '\t', '\n', 'r'));
 
-  auto letter         = alex::from('a').to('z') || alex::from('A').to('Z');
-  auto digit          = alex::from('0').to('9');
+  auto letter         = from('a').to('z') || from('A').to('Z');
+  auto digit          = from('0').to('9');
 
-  auto digits         = alex::repeat(digit);
-  auto int_num        = '0' || (alex::from('1').to('9') + digits);
-  auto float_num      = int_num + alex::optional('.' + digits);
+  auto digits         = repeat(digit);
+  auto int_num        = '0' || (from('1').to('9') + digits);
+  auto float_num      = int_num + optional('.' + digits);
   auto sign           = '-';
-  auto power          = alex::one_of('e', 'E') + alex::optional('+', '-') + digits;
-  auto number         = alex::optional(sign) + float_num + alex::optional(power);
+  auto power          = one_of('e', 'E') + optional('+', '-') + digits;
+  auto number         = optional(sign) + float_num + optional(power);
 
-  auto string_char    = ('\\' + alex::any()) || alex::any_but('"');
-  auto string_content = alex::repeat(string_char);
-  auto string_literal = '"' + string_content + '"';
+  auto string_literal = '"' + repeat(('\\' + any()) || any_but('"')).until('"');
 
-  auto boolean        = alex::one_of("true", "false");
+  auto keyword        = one_of("true", "false", "null");
 
-  auto object         = alex::recursive();
-  auto array          = alex::recursive();
+  auto object         = recursive();
+  auto array          = recursive();
 
-  auto expression     = (object || array || number || string_literal || boolean || "null");
+  auto expression     = (object || array || number || string_literal || keyword);
 
-  auto field_name     = string_literal;
-  auto colon          = ws + ':' + ws;
   auto comma          = ws + ',' + ws;
-  auto field          = field_name + colon + expression;
+       array          = '[' + ws + repeat(expression).separator(comma) + ws + ']';
 
-  object              = '{' + ws + alex::repeat(field).separator(comma) + ws + '}';
-  array               = '[' + ws + alex::repeat(expression).separator(comma) + ws + ']';
+  auto colon          = ws + ':' + ws;
+  auto field          = string_literal + colon + expression;
+       object         = '{' + ws + repeat(field).separator(comma) + ws + '}';
 
-  std::cout << alex::parse("0",      number) << "\n";                        // true
-  std::cout << alex::parse("-1",     number) << "\n";                        // true
-  std::cout << alex::parse("3.141",  number) << "\n";                        // true
-  std::cout << alex::parse("-1.5e4", number) << "\n";                        // true
-                                                                             
-  std::cout << alex::parse("[\"foo\",-0.5,true]", array) << "\n";            // true
-  std::cout << alex::parse("[ false , 1e10 ]",    array) << "\n";            // true
-                                                                             
-  std::cout << alex::parse("{ \"foo\" : \"bar\" }",     object) << "\n";     // true
-  std::cout << alex::parse("{\"foo\": null, \"x\": 0}", object) << "\n";     // true
+  auto json           = ws + expression + ws;
 
-  std::cout << alex::parse("{\"outer\" : {\"inner\" : 0}}", object) << "\n"; // true
+  std::cout << parse("0",      json) << "\n";                        // true
+  std::cout << parse("-1",     json) << "\n";                        // true
+  std::cout << parse("3.141",  json) << "\n";                        // true
+  std::cout << parse("-1.5e4", json) << "\n";                        // true
+                                                                       
+  std::cout << parse("[\"foo\",-0.5,true]", json) << "\n";           // true
+  std::cout << parse("[ false , 1e10 ]",    json) << "\n";           // true
+                                                                       
+  std::cout << parse("{ \"foo\" : \"bar\" }",     json) << "\n";     // true
+  std::cout << parse("{\"foo\": null, \"x\": 0}", json) << "\n";     // true
 
-  std::cout << alex::parse("[\"outer\", [\"inner\"]]", array) << "\n";       // true
+  std::cout << parse("{\"outer\" : {\"inner\" : 0}}", json) << "\n"; // true
+
+  std::cout << parse("[\"outer\", [\"inner\"]]", json) << "\n";      // true
 }
