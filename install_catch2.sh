7db454750f14751192daf9e586edae02e17384bd
Luc Ruijsink <lruijsink@gmail.com>
2020-10-01 00:57:39 +0200

    Don't use substitutions

--- install_catch2.sh
+++ install_catch2.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/catchorg/Catch2.git
 cd Catch2
-cmake -B${{runner.workspace}}/build -H. -DBUILD_TESTING=OFF
-sudo cmake --build ${{runner.workspace}}/build/ --target install
+cmake -Bbuild -H. -DBUILD_TESTING=OFF
+sudo cmake --build build/ --target install
