9c170639b1856d40fb7453b4266cb5c6258aabbe
Luc Ruijsink <lruijsink@gmail.com>
2020-10-01 03:19:55 +0200

    Set up Clang format action

--- test/test_basic.cpp
+++ test/test_basic.cpp
@@ -1,10 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-TEST_CASE("Test", "[basic]") {
-  REQUIRE(true);
-}
+TEST_CASE("Test", "[basic]") { REQUIRE(true); }
 
-TEST_CASE("Test failure", "[basic]") {
-  REQUIRE(false);
-}
+TEST_CASE("Test failure", "[basic]") { REQUIRE(false); }
