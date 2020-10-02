e480caba9070656730df3cf1ccd5f46ec7efdb67
Luc Ruijsink <lruijsink@gmail.com>
2020-10-02 02:10:06 +0200

    Updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,16 @@
-COMPILER=$1
-TARGET=$2
+COMPILER=${1:-g++}
+TARGET=${2:-Release}
+PROJECT_ROOT=${3:-.}
+OUTPUT_DIR=$PROJECT_ROOT/${4:-build}
 
-mkdir build
-cd build
+echo Compiling with $COMPILER
+echo Setting target to $TARGET
+echo Building directory $PROJECT_ROOT
+echo Outputting to $OUTPUT_DIR
+
+cd $PROJECT_ROOT
+mkdir -p $OUTPUT_DIR
+cd $OUTPUT_DIR
 cmake -DCMAKE_BUILD_TYPE=$TARGET -DCMAKE_CXX_COMPILER=$COMPILER ..
 cmake --build . --config $TARGET
 ctest -C $BUILD_TYPE
