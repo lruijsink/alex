b967467c357e4e5382bfe2c5a4372f3c5473d4b4
Luc Ruijsink <lruijsink@gmail.com>
2020-10-02 03:20:10 +0200

    Undo clang-format for bash scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,18 @@
-COMPILER = ${1 : -g++} TARGET = ${2 : -Release} PROJECT_ROOT = ${3 : -.}
+COMPILER=${1:-g++}
+TARGET=${2:-Release}
+PROJECT_ROOT=${3:-.}
 
-echo Setting compiler = $COMPILER echo Setting target =
-    $TARGET echo Setting project root =
-        $PROJECT_ROOT
+echo Setting compiler = $COMPILER
+echo Setting target = $TARGET
+echo Setting project root = $PROJECT_ROOT
 
-            mkdir -
-        p $PROJECT_ROOT / build pushd $PROJECT_ROOT /
-            build
+mkdir -p $PROJECT_ROOT/build
+pushd $PROJECT_ROOT/build
 
-                cmake -
-        DCMAKE_BUILD_TYPE =
-            $TARGET - DCMAKE_CXX_COMPILER =
-                $COMPILER..cmake-- build.--config $TARGET ctest -
-                C $BUILD_TYPE CTEST_RESULT =
-                    $ ?
+cmake -DCMAKE_BUILD_TYPE=$TARGET -DCMAKE_CXX_COMPILER=$COMPILER ..
+cmake --build . --config $TARGET
+ctest -C $BUILD_TYPE
+CTEST_RESULT=$?
 
-                      popd exit $CTEST_RESULT
+popd
+exit $CTEST_RESULT
