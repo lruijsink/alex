33bb5167d4985c924ddb1e3e69a6a216ab0ec591
Luc Ruijsink <lruijsink@gmail.com>
2020-10-02 02:54:23 +0200

    Slightly improved build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,18 @@
 COMPILER=${1:-g++}
 TARGET=${2:-Release}
 PROJECT_ROOT=${3:-.}
-OUTPUT_DIR=${4:-build}
 
-echo Compiling with $COMPILER
-echo Setting target to $TARGET
-echo Building directory $PROJECT_ROOT
-echo Outputting to $OUTPUT_DIR
+echo Setting compiler = $COMPILER
+echo Setting target = $TARGET
+echo Setting project root = $PROJECT_ROOT
 
-cd $PROJECT_ROOT
-mkdir -p $OUTPUT_DIR
-cd $OUTPUT_DIR
+mkdir -p $PROJECT_ROOT/build
+pushd $PROJECT_ROOT/build
+
 cmake -DCMAKE_BUILD_TYPE=$TARGET -DCMAKE_CXX_COMPILER=$COMPILER ..
 cmake --build . --config $TARGET
 ctest -C $BUILD_TYPE
 CTEST_RESULT=$?
-cd ..
+
+popd
 exit $CTEST_RESULT
